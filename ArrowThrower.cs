75cf2de0120688fa31ce83e4515ef63dd6e3521c
Mafffie Cohen <111717714+maffiemaffie@users.noreply.github.com>
2023-03-28 11:24:49 -0400

    Add files via upload

--- ArrowThrower.cs
+++ ArrowThrower.cs
@@ -2,8 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThrowArrow : MonoBehaviour
+public class ArrowThrower : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject arrow;
+
+    [SerializeField]
+    private float duration = 0.1f; // 100 ms delay
+
+    [SerializeField]
+    private Vector3 center = Vector3.zero;
+
+    [SerializeField]
+    private float spread = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +26,12 @@ public class ThrowArrow : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Throw(float to)
+    {
+        float angle = Random.Range(0, Mathf.PI * 2);
+
+        GameObject thisArrow = Instantiate(arrow, center, Quaternion.identity);
     }
 }
